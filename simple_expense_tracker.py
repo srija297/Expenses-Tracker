a641ad335a913a9386d5cdce4c9bfc05a6f23dd5
Marikannan Perumal <marik7939@gmail.com>
2025-07-19 15:59:27 +0530

    Add: Main function and other feature structure to the main branch of the repo

--- simple_expense_tracker.py
+++ simple_expense_tracker.py
@@ -1,4 +1,11 @@
+expenses = []  # Global list to store expense dictionaries
 
+def add_expense():
+    
+    
+def view_total():
+   
+    
 
 def list_expenses():
     
@@ -9,4 +16,37 @@ def list_expenses():
     for idx, expense in enumerate(expenses, 1):
         print(f"{idx}. ₹{expense['amount']:.2f} - {expense['category']} - {expense['description']}")
     print()
+
+def filter_by_category():
+    
+    
+
+def main():
+    
+    print(" Welcome to Expense Tracker")
+    while True:
+        print("1. Add Expense")
+        print("2. View Total")
+        print("3. List Expenses")
+        print("4. Filter by Category")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+        print()
+
+        if choice == '1':
+            add_expense()
+        elif choice == '2':
+            view_total()
+        elif choice == '3':
+            list_expenses()
+        elif choice == '4':
+            filter_by_category()
+        elif choice == '5':
+            print(" Exiting... Thank you for using Expense Tracker!")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 5.\n")
+
+if __name__ == "__main__":
+    main()
 
